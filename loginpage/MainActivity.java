6661ed13174a0fafcb4d29ea24072333ee5a38e8
saudyusaf <saudyusaf289@gmail.com>
2025-09-24 09:27:29 +0530

    Create MainActivity.java

---
+++ loginpage/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.loginpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText username = findViewById(R.id.edit1);
+        EditText password = findViewById(R.id.edit2);
+        Button Lbutton =  findViewById(R.id.btn);
+        Lbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin123")) {
+                    Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
+
+    }
+}
