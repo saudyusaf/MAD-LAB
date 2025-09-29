3be09358ddec6ffee3eb190bd4b46748c1ba3eed
saudyusaf <saudyusaf289@gmail.com>
2025-09-29 12:21:38 +0530

    Create MainActivity.java

---
+++ intend/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.intent1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void OnIndexSendButtonClicked(View view) {
+        Intent intent = new Intent(this, MainActivity2.class);
+
+        EditText editText = (EditText) findViewById(R.id.text);
+        String index = editText.getText().toString();
+
+        intent.putExtra("ImageIndex", index);
+        startActivity(intent);
+    }
+}
